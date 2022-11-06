aed5ddcef96145e9aa6cee207f69d096987946b7
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-11-06 11:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
       // 2022> 12 => true
       const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
       calculateChange(amountToBeReturned);
-     
+     showMessage ("collect your balance")
     } else {
       showMessage("Do you wanna wash plates?");
     }
